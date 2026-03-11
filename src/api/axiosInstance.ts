f4435cd0c182698d3e37e0505e5369ae9c588a61
Atul <atul5465king@gmail.com>
2026-03-12 03:37:59 +0530

    updated url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://15.206.173.5:8080/api',
   timeout: 60000,
   headers: { 'Content-Type': 'application/json' },
 });
