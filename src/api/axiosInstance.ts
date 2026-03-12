77a0c595e13806e14728de57f72f6d459b14fe76
Atul <atul5465king@gmail.com>
2026-03-12 19:38:40 +0530

    updated url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:8080/api',
-  baseURL: 'http://15.206.173.5:8080/api',
+  baseURL: 'https://prephub.atul.codes/api',
   timeout: 60000,
   headers: { 'Content-Type': 'application/json' },
 });
