9c38f3bfc840d7d0ca66b925cea7fa28cde627a2
Harshit8384 <harshit.prasad.code@gmail.com>
2026-03-14 10:41:50 +0530

    logo change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
-import pdfjsLib from "pdfjs-dist";
+import * as pdfjsLib from "pdfjs-dist";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
