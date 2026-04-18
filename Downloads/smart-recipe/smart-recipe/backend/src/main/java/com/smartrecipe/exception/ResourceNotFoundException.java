2cf8d4bef5ae0be3e670625ae8589d17299ba670
Atul Kumar Rana <143958140+Atul-Kumar-Rana@users.noreply.github.com>
2026-04-18 17:36:18 +0530

    Delete Downloads/smart-recipe/smart-recipe directory

--- Downloads/smart-recipe/smart-recipe/backend/src/main/java/com/smartrecipe/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.smartrecipe.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
